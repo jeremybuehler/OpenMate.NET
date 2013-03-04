e59801669429c318b24ad5d15c66bedb597823a5
Matt Wagner <mattgwagner@gmail.com>
2013-03-04 16:51:53 -0500

    Switched to WCF references instead of wsdl generated and put in some boilerplate code.

---
+++ OpenMateNET.Lib/OpenMateAPI.cs
@@ -0,0 +1,129 @@
+﻿using OpenMateNET.Lib.ProcessEventService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenMateNET.Lib
+{
+    public interface IOpenMateAPI
+    {
+
+    }
+
+    public class OpenMateAPI : IOpenMateAPI
+    {
+        // What version of the STAR standard we use.
+        public const String PAYLOAD_VERSION = "STAR-5.5.4";
+
+        /// <summary>
+        /// The Url of the web service end point
+        /// </summary>
+        public String Url { get; private set; }
+
+        /// <summary>
+        /// The username to authenticate with the web service.
+        /// </summary>
+        public int ThirdPartySourceId { get; private set; }
+
+        /// <summary>
+        /// The password to authenticate with the web service.
+        /// </summary>
+        public String Password { get; private set; }
+
+        public OpenMateAPI(String Url, int ThirdPartySourceId, String Password, int ThirdPartyId)
+        {
+            this.Url = Url;
+            this.ThirdPartySourceId = ThirdPartySourceId;
+            this.Password = Password;
+        }
+
+        public virtual void Test()
+        {
+            using (var svc = GetService())
+            {
+                var response = svc.processEvent(
+                    // Add in the authentication and versioning information
+                    new authenticationToken()
+                    {
+                        userName = this.ThirdPartySourceId.ToString(),
+                        password = this.Password
+                    },
+                    this.ThirdPartySourceId,
+
+                    // Which dealership to target
+                    1,
+
+                    // Which transaction to run
+                    transactionType.GetCustomers,
+
+                    // The data to be processed
+                    "",
+
+                     PAYLOAD_VERSION
+                );
+
+                // Check the response for errors and handle them as necessary.
+                CheckErrors(response);
+
+                // Process the response.
+
+
+            }
+        }
+
+        internal virtual void CheckErrors(processEventResult result)
+        {
+            /*
+                <ns2:processEventResult>
+                <!-- Response Payload -->
+                <response>Request unauthorized</response>
+                <!-- Was the event rejected due to a business validation problem? -->
+                <businessError>true</businessError>
+                <!-- Was the event rejected due to a system problem? -->
+                <systemError>false</systemError>
+                <!-- Should this event be reposted? -->
+                <retryable>false</retryable>
+                <!-- What was the specific problem with the event? SUCCESS if none. -->
+                <statusCode>PERMISSION_DENIED</statusCode>
+                </ns2:processEventResult>
+            */
+
+            // Check the status codes.
+            if (result.statusCode == processEventStatusCode.ENDPOINT_FAILURE)
+            {
+
+            }
+
+            // Check for a system error.
+            if (result.systemError)
+            {
+
+            }
+
+            // Check for a business error.
+            if (result.businessError)
+            {
+
+            }
+
+            if (result.retryable)
+            {
+                // Re-send it.
+
+            }
+
+            // Otherwise the request was successful, move on.
+        }
+
+        internal virtual ProcessEventClient GetService()
+        {
+            // TODO Set any special binding settings here.
+            var binding = new BasicHttpBinding() { };
+
+            return new ProcessEventClient(binding, new EndpointAddress(this.Url));
+        }
+    }
+}
--- OpenMateNET.TestApp/Program.cs
+++ OpenMateNET.TestApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenMateNET.Lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace OpenMateNET.TestApp
     {
         static void Main(string[] args)
         {
+            // TODO
         }
     }
 }
